c6928418a6a09ee915bc4e978e73546fde6e303c
Adrian <mail@adrianrostin.de>
2025-09-04 15:01:41 +0200

    fixed formatting errors

--- src/main/java/com/ase/userservice/components/Embedded.java
+++ src/main/java/com/ase/userservice/components/Embedded.java
@@ -11,80 +11,80 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Embedded {
-    @Embeddable
-    @Data
-    public static class Content {
-      @Enumerated(EnumType.STRING)
-      private ContentFormat format;
+  @Embeddable
+  @Data
+  public static class Content {
+    @Enumerated(EnumType.STRING)
+    private ContentFormat format;
 
-      @Column(columnDefinition = "TEXT")
-      private String body;
+    @Column(columnDefinition = "TEXT")
+    private String body;
 
-      public enum ContentFormat {
-        markdown, html
-      }
+    public enum ContentFormat {
+      markdown, html
     }
+  }
 
-    @Embeddable
-    @Data
-    public static class FeaturedImage {
-      private String url;
-      private String altText;
-      private String caption;
+  @Embeddable
+  @Data
+  public static class FeaturedImage {
+    private String url;
+    private String altText;
+    private String caption;
+  }
+
+  @Embeddable
+  @Data
+  public static class Author {
+    private String userId;
+    private String name;
+    private String avatarUrl;
+  }
+
+  @Embeddable
+  @Data
+  public static class Expiration {
+    private LocalDateTime expiresAt;
+    private Boolean autoArchive;
+  }
+
+  @Embeddable
+  @Data
+  public static class Permissions {
+    @ElementCollection
+    @CollectionTable(name = "news_post_read_permissions")
+    private List<PrincipalRef> read;
+
+    @ElementCollection
+    @CollectionTable(name = "news_post_write_permissions")
+    private List<PrincipalRef> write;
+
+    @ElementCollection
+    @CollectionTable(name = "news_post_delete_permissions")
+    private List<PrincipalRef> delete;
+  }
+
+  @Embeddable
+  @Data
+  public static class PrincipalRef {
+    private String id;
+
+    @Enumerated(EnumType.STRING)
+    private PrincipalType type;
+
+    private String name;
+
+    public enum PrincipalType {
+      user, group
     }
+  }
 
-    @Embeddable
-    @Data
-    public static class Author {
-      private String userId;
-      private String name;
-      private String avatarUrl;
-    }
-
-    @Embeddable
-    @Data
-    public static class Expiration {
-      private LocalDateTime expiresAt;
-      private Boolean autoArchive;
-    }
-
-    @Embeddable
-    @Data
-    public static class Permissions {
-      @ElementCollection
-      @CollectionTable(name = "news_post_read_permissions")
-      private List<PrincipalRef> read;
-
-      @ElementCollection
-      @CollectionTable(name = "news_post_write_permissions")
-      private List<PrincipalRef> write;
-
-      @ElementCollection
-      @CollectionTable(name = "news_post_delete_permissions")
-      private List<PrincipalRef> delete;
-    }
-
-    @Embeddable
-    @Data
-    public static class PrincipalRef {
-      private String id;
-
-      @Enumerated(EnumType.STRING)
-      private PrincipalType type;
-
-      private String name;
-
-      public enum PrincipalType {
-        user, group
-      }
-    }
-
-    @Embeddable
-    @Data
-    public static class Settings {
-      private Boolean featured;
-      private Boolean sticky;
-    }
+  @Embeddable
+  @Data
+  public static class Settings {
+    private Boolean featured;
+    private Boolean sticky;
+  }
 
 }
 
