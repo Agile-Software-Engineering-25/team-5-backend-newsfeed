8013b0045f19102b8e903cf1c9aba01dc75cc592
Hannes <hanpie0501@gmail.com>
2025-09-18 16:01:11 +0200

    merge clean up

--- src/main/java/com/ase/newsfeedservice/repositories/NewsPostRepository.java
+++ src/main/java/com/ase/newsfeedservice/repositories/NewsPostRepository.java
@@ -23,12 +23,18 @@ import org.springframework.data.domain.Pageable;
 >>>>>>> 4238373 (fixed errors):src/main/java/com/ase/userservice/repositories/NewsPostRepository.java
 =======
 
+<<<<<<< HEAD:src/main/java/com/ase/newsfeedservice/repositories/NewsPostRepository.java
 import java.time.OffsetDateTime;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 >>>>>>> f85c7a6 (fixed errors):src/main/java/com/ase/userservice/repositories/NewsPostRepository.java
+<<<<<<< HEAD
 >>>>>>> f1cd5ae (fixed errors)
+=======
+=======
+>>>>>>> fa7e44b (merge clean up):src/main/java/com/ase/userservice/repositories/NewsPostRepository.java
+>>>>>>> 20ed3a2 (merge clean up)
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
