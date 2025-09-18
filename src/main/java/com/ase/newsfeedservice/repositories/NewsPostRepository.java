9d941ddad49d1e321986b0bb79834903b6c0b5b0
Hannes <hanpie0501@gmail.com>
2025-09-18 16:46:30 +0200

    linter

--- src/main/java/com/ase/newsfeedservice/repositories/NewsPostRepository.java
+++ src/main/java/com/ase/newsfeedservice/repositories/NewsPostRepository.java
@@ -40,14 +40,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface NewsPostRepository extends JpaRepository<NewsPost, String> {
-    @Query("SELECT np FROM NewsPost np WHERE " +
-            "(:query IS NULL OR LOWER(np.title) LIKE LOWER(CONCAT('%', :query, '%')) OR LOWER(np.summary) LIKE LOWER(CONCAT('%', :query, '%'))) AND "
-            +
-            "(:from IS NULL OR np.publishDate >= :from) AND " +
-            "(:to IS NULL OR np.publishDate <= :to)")
-    Page<NewsPost> listNewsPosts(
-            @Param("query") String query,
-            @Param("from") OffsetDateTime from,
-            @Param("to") OffsetDateTime to,
-            Pageable pageable);
+  @Query("SELECT np FROM NewsPost np WHERE " +
+      "(:query IS NULL OR LOWER(np.title) LIKE LOWER(CONCAT('%', :query, '%')) OR LOWER(np.summary) LIKE LOWER(CONCAT('%', :query, '%'))) AND "
+      +
+      "(:from IS NULL OR np.publishDate >= :from) AND " +
+      "(:to IS NULL OR np.publishDate <= :to)")
+  Page<NewsPost> listNewsPosts(
+      @Param("query") String query,
+      @Param("from") OffsetDateTime from,
+      @Param("to") OffsetDateTime to,
+      Pageable pageable);
 }
