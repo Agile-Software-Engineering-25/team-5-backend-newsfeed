0c3a460b5a1ddcbe471d52bba1f78f1e029fa0b4
Adrian <mail@adrianrostin.de>
2025-09-25 08:59:02 +0200

    removed unused import

--- src/main/java/com/ase/newsfeedservice/components/NewsPost.java
+++ src/main/java/com/ase/newsfeedservice/components/NewsPost.java
@@ -4,7 +4,6 @@ import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
-import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
