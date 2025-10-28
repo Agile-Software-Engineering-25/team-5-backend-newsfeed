d3def43b6b6b975cbdf771434be505824c03c8ce
Hannes <hanpie0501@gmail.com>
2025-10-28 13:05:56 +0100

    use config from src repo

---
+++ src/main/java/com/ase/newsfeedservice/config/JwtAuthConverter.java
@@ -0,0 +1,20 @@
+package com.ase.newsfeedservice.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.lang.NonNull;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class JwtAuthConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(@NonNull Jwt jwt) {
+        var roles = jwt.getClaimAsStringList("groups");
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("" + role))
+                .collect(Collectors.toList());
+    }
+}
--- src/main/java/com/ase/newsfeedservice/config/ProdSecurityConfig.java
+++ src/main/java/com/ase/newsfeedservice/config/ProdSecurityConfig.java
@@ -5,16 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /*
 Area-2.Team-5.Read.NewsPost-Engineering 
@@ -32,38 +24,19 @@ lecturer
 public class ProdSecurityConfig {
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+    jwtConverter.setJwtGrantedAuthoritiesConverter(new JwtAuthConverter());
+
+    // the role always has to be capitalized
     http
+        .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> auth
         .requestMatchers("/**")
         .hasAnyAuthority("sau-admin", "university-administrative-staff")
         .anyRequest().denyAll())
         .oauth2ResourceServer(oauth2 -> oauth2
-        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)));
     return http.build();
-  }
-
-  @Bean
-  public JwtAuthenticationConverter jwtAuthenticationConverter() {
-    JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-    converter.setJwtGrantedAuthoritiesConverter(this::extractRealmRoles);
-    return converter;
-  }
-
-  private Collection<GrantedAuthority> extractRealmRoles(Jwt jwt) {
-    Map<String, Object> realmAccess = jwt.getClaim("realm_access");
-    if (realmAccess == null) {
-      return List.of();
-    }
-
-    Object rolesObj = realmAccess.get("roles");
-    if (!(rolesObj instanceof List<?> roles)) {
-      return List.of();
-    }
-
-    return roles.stream()
-        .filter(r -> r instanceof String)
-        .map(r -> new SimpleGrantedAuthority("" + r))
-        .collect(Collectors.toList());
   }
 }
