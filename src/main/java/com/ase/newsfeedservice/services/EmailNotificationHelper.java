c75409579739d476599089d6a260ee5aaa5627de
Mirco-Leuchtmann <mirco.leuchtmann@stud-provadis-hochschule.de>
2025-10-30 13:12:08 +0100

    EmailNotificationHelper implementiert, groups müssen noch übergeben werden.

--- src/main/java/com/ase/newsfeedservice/services/EmailNotificationHelper.java
+++ src/main/java/com/ase/newsfeedservice/services/EmailNotificationHelper.java
@@ -1,8 +1,6 @@
 package com.ase.newsfeedservice.services;
 
 import com.ase.newsfeedservice.components.NewsPost;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -17,20 +15,32 @@ public class EmailNotificationHelper {
   // URLs can be injected via application.yml if you prefer
   private static final String TOKEN_URL = "https://keycloak.sau-portal.de/realms/sau/protocol/openid-connect/token";
   private static final String API_URL = "https://sau-portal.de/notification-service/api/v1/notifications";
-  private static final String clientId = "team-5";
-  private static final String clientSecret = "";
+  private static final String clientId = System.getenv("CLIENT_ID");
+  private static final String clientSecret = System.getenv("CLIENT_SECRET");
 
   public String emailNotify(NewsPost newsPost) {
     String jwt = getJwtToken(clientId, clientSecret);
 
+    Map<String, Object> requestBody = Map.of(
+        "title", "Neuer Beitrag im Newsfeed",
+        "message", "Es wurde ein neuer Beitrag veröffentlicht: " + newsPost.getTitle(),
+        "notifyType", "All",
+        "notificationType", "Info",
+        "groups", ""
+    );
+
+    // idk ob das permissions array nur die groups hält
+    requestBody.put("groups", newsPost.getPermissions());
+
     HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
     headers.setBearerAuth(jwt);
 
-    HttpEntity<Void> entity = new HttpEntity<>(headers);
+    HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
 
     ResponseEntity<String> response = restTemplate.exchange(
         API_URL,
-        HttpMethod.GET,
+        HttpMethod.POST,
         entity,
         String.class
     );
@@ -38,7 +48,7 @@ public class EmailNotificationHelper {
     return response.getBody();
   }
 
-  private String getJwtToken(String clientId, String clientSecret) {
+  private String getJwtToken() {
     Map<String, String> requestBody = Map.of(
         "client_id", clientId,
         "client_secret", clientSecret,
