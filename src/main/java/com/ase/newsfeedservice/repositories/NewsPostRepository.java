39933348798d8b58811d843582408396ddbb38f4
Hannes <hanpie0501@gmail.com>
2025-11-05 17:23:31 +0100

    explicitly convert column to string

--- src/main/java/com/ase/newsfeedservice/repositories/NewsPostRepository.java
+++ src/main/java/com/ase/newsfeedservice/repositories/NewsPostRepository.java
@@ -15,7 +15,7 @@ public interface NewsPostRepository extends JpaRepository<NewsPost, String> {
         SELECT DISTINCT np FROM NewsPost np
         JOIN np.permissions perm
         WHERE
-        (:query IS NULL OR np.title LIKE CONCAT('%', :query, '%'))
+        (:query IS NULL OR LOWER(np.title) LIKE LOWER(CONCAT('%', :query, '%')))
         AND (:from IS NULL OR np.creation_date >= :from)
         AND (:to IS NULL OR np.creation_date <= :to)
         AND (perm IN :groups)
