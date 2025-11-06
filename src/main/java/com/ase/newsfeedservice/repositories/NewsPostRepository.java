7088b2182fb4be67448dcf2e342b8f1b7b14cb45
Hannes Piechulek <hannes.piechulek@telekom.de>
2025-11-06 09:45:55 +0100

    change query to ILIKE

--- src/main/java/com/ase/newsfeedservice/repositories/NewsPostRepository.java
+++ src/main/java/com/ase/newsfeedservice/repositories/NewsPostRepository.java
@@ -15,11 +15,11 @@ public interface NewsPostRepository extends JpaRepository<NewsPost, String> {
         SELECT DISTINCT np FROM NewsPost np
         JOIN np.permissions perm
         WHERE
-        (:from IS NULL OR np.creation_date >= :from)
+        (:query IS NULL OR np.title ILIKE CONCAT('%', :query, '%'))
+        AND (:from IS NULL OR np.creation_date >= :from)
         AND (:to IS NULL OR np.creation_date <= :to)
         AND (perm IN :groups)
         """)
-        // (:query IS NULL OR LOWER(np.title) LIKE LOWER(CONCAT('%', :query, '%')))
   Page<NewsPost> listNewsPosts(
       @Param("query") String query,
       @Param("from") OffsetDateTime from,
